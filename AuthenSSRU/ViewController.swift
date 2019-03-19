087b739decca91466241530eec510aa224ca7e11
masterUNG <masterUNG@abc.com>
2019-03-19 16:08:01 +0700

    Initial Commit

---
+++ AuthenSSRU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthenSSRU
+//
+//  Created by Teacher on 19/3/2562 BE.
+//  Copyright © 2562 Suthira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
