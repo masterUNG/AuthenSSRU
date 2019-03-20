bdeb3920b16fe5c9e9bc96b32d34b5998222ea46
masterUNG <masterUNG@abc.com>
2019-03-20 16:13:04 +0700

    Cloud Button Success

--- AuthenSSRU/RegisterViewController.swift
+++ AuthenSSRU/RegisterViewController.swift
@@ -18,6 +18,11 @@ class RegisterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }   // Main Method
     
+    
+    @IBAction func uploadButton(_ sender: UIBarButtonItem) {
+    }
+    
+    
 
     @IBAction func backButton(_ sender: UIBarButtonItem) {
         
